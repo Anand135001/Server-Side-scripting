88c88e076bb8ce2dfe0fd472b5c08fa862ffacb9
Anand <160326979+Anand135001@users.noreply.github.com>
2024-12-27 17:30:35 +0530

    Encryption

--- JWT-Auth/app.js
+++ JWT-Auth/app.js
@@ -1,17 +1,27 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 
-app.use(cookieParser());
+// const cookieParser = require('cookie-parser');
+// app.use(cookieParser());
 
+// app.get('/', (req, res) => {
+//   res.cookie("name", "anand");
+//   res.send('main');
+// })
+
+// app.get('/read', (req, res) => {
+//   console.log(req.cookies);
+//   res.send('read');
+// })
+
+// ==== encryption ===
 app.get('/', (req, res) => {
-  res.cookie("name", "anand");
-  res.send('main');
-})
-
-app.get('/read', (req, res) => {
-  console.log(req.cookies);
-  res.send('read');
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash("dnakjkj", salt, function (err, hash) {
+      console.log(hash);
+    });
+  });
+  res.send("dcrypt");
 })
 
 app.listen(3000);
