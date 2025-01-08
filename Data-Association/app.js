c429b1559980dee2901fa8115279e363af4e264b
Anand <aanadmainwal@gmail.com>
2025-01-08 13:32:35 +0530

    create post route

--- Data-Association/app.js
+++ Data-Association/app.js
@@ -1,9 +1,35 @@
 const express = require('express')
 const app = express()
+const userModel = require('./model/user');
+const postModel = require('./model/post');
+const user = require('./model/user');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
+app.get('/create', async (req, res) => {
+    let user = await userModel.create({
+        name: 'John',
+        email: 'John@gmail.com',
+        age: 25
+    })
 
-app.listen(3000) 
+    res.send(user);
+})
+
+app.get('/createpost', async (req, res) => {
+    let post = await postModel.create({
+      postdata: "This is a post",
+      user: "677e2ef296a91b9a29c9f72f",
+      date: Date.now(),
+    });
+    
+    let user = await userModel.findOne({ _id: "677e2ef296a91b9a29c9f72f" });
+    user.posts.push(post._id);
+    user.save();
+
+    res.send({post, user});
+})
+
+app.listen(3000); 
