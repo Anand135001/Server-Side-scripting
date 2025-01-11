ca3b65a3dc964bc4049c471558f6e7725c35c6d6
Anand <aanadmainwal@gmail.com>
2025-01-11 12:43:01 +0530

    implement user login, logout, and profile routes with JWT authentication

--- Post-App/app.js
+++ Post-App/app.js
@@ -24,11 +24,8 @@ app.post('/create', async(req, res) => {
   
   if(user) return res.send("User Already registered");
   
-  const saltRounds = 10;
-  const PlaintextPassword = '#@and!*#';
-
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    bcrypt.hash(PlaintextPassword, salt, async(err, hash) => {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async(err, hash) => {
          
       let user = await userModel.create({
           username,
@@ -46,4 +43,52 @@ app.post('/create', async(req, res) => {
   });
 });
 
+app.get('/login', (req, res) => {
+   res.render('login');
+})
+
+app.post('/login', async(req, res) => {
+  let { email, password } = req.body;
+
+  let user = await userModel.findOne({email});
+  if (!user) return res.send("Somthing went wrong");
+  bcrypt.compare(password, user.password, (err, result) => {
+      if(result) {
+        let token = jwt.sign({email, userid: user._id},'scretekey');
+        res.cookie('token', token);
+        res.status(200).send("login successful");
+      }
+      else res.redirect('/login');
+  });
+
+})
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.send("logged out");
+});
+
+
+app.get('/profile', isloggedIn, async(req, res) => {
+  console.log(req.user);     
+  res.send("login");
+});
+
+
+function isloggedIn(req, res, next){
+ try{
+  
+    let token = req.cookies.token;
+    if(!token){
+      res.send("You are not logged in");
+    }
+    else{
+      let data = jwt.verify(token, 'scretekey'); 
+      req.user = data;
+      next();
+    }
+  }catch(err){
+     res.send("Login required");
+  } 
+}
 app.listen(3000);
