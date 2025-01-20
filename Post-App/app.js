7179f90e79358671314d5b011bf70dbd6c39eb38
Anand <aanadmainwal@gmail.com>
2025-01-20 11:25:22 +0530

    implement image upload functionality with multer and create test view

--- Post-App/app.js
+++ Post-App/app.js
@@ -1,6 +1,9 @@
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken'); 
+const multer = require('multer');
+const crypto =  require('crypto');
+const path = require('path');
 const userModel = require("./models/user");
 const postModel = require('./models/post');
 
@@ -13,6 +16,20 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/uploads");
+  },
+  filename: function (req, file, cb) {
+  crypto.randomBytes(12, function(err, bytes){
+    const fileN = bytes.toString('hex') + path.extname(file.originalname);
+    cb(null, fileN);
+  })  
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
    res.render('index');
@@ -112,6 +129,18 @@ app.post('/update/:id', isloggedIn, async(req, res) => {
     res.redirect('/profile');
 });
 
+app.get('/test', (req, res) => {
+  res.render('test');
+});
+
+app.post("/upload", upload.single('image'), (req, res) => {
+   console.log(req.file);
+  //  res.redirect('/test');
+});
+
+
+
+
 function isloggedIn(req, res, next){
  try{
   
@@ -128,4 +157,5 @@ function isloggedIn(req, res, next){
      res.send("Login required");
   } 
 }
+
 app.listen(3000);
